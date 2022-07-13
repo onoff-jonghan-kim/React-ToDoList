fae61e8167e5f75aaac33ae4f98066290ece2948
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-07-13 15:57:40 +0900

    #5.13 fin

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "./atoms";
 
-function ToDo({text, category}:IToDo) {
+function ToDo({text, category, id}:IToDo) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event : React.MouseEvent<HTMLButtonElement>) => {
     const {currentTarget: {name},
-  } = event;
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = {text, id, category:name};
+      console.log(oldToDo,newToDo);
+      return oldToDos;
+    });
   };
   return (
     <li>
